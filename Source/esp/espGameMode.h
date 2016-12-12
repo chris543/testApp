d2395331f9c70bee0d22384f88b1f754ed736925
chris543 <dshapepicture@yahoo.com.tw>
2016-12-12 11:47:15 +0800

    initial commit

---
+++ Source/esp/espGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "espGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESP_API AespGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
