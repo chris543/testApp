f15fa4076c815f5aa1cd3a5bdd98fb3d0611c171
chris543 <atw543@gmail.com>
2017-03-08 17:45:28 +0800

    adfadf

--- rename.c
+++ rename.c
@@ -7,3 +7,16 @@ hello
 ,,,.....
     ble_our_service_on_ble_evt(&m_our_service, p_ble_evt);
 CB1n/source/CB1n_project/CB1n/pca10040/s132/iar/_build/
+
+
+
+}
+static void Output_Via_BLE(uint8_t *data) {
+	data[data[1] + 2] = calculateCheckSum(data);
+	sendDataToBLE(&m_our_service, data);
+}
+
+void Output_Via_UART(uint8_t *data) {
+	data[data[1] + 2] = calculateCheckSum(data);
+
+	for (int i = 0; i < data[1] +
---
+++ test1 - Copy.c
@@ -0,0 +1,44 @@
+#include <C8051F310.h>
+#include <math.h>
+
+//-----------------------------------------------------------------------------
+// Global CONSTANTS
+//-----------------------------------------------------------------------------
+
+
+#define  SYSCLK         24500000       	// System clock frequency in Hz
+
+#define  SMB_FREQUENCY  50000          	// Target SCL clock rate
+                                       	// This example supports between 10kHz
+                                       	// and 100kHz
+
+#define  WRITE          0x00           	// SMBus WRITE command
+#define  READ           0x01           	// SMBus READ command
+
+// Device addresses (7 bits, lsb is a don't care)
+#define TSL2550			0x72			// TSL2550 light sensor address for slave target
+
+// Status vector - top 4 bits only
+#define  SMB_MTSTA      0xE0           	// (MT) start transmitted
+#define  SMB_MTDB       0xC0           	// (MT) data byte transmitted
+#define  SMB_MRDB       0x80           	// (MR) data byte received
+// End status vector definition
+#if (SPIS2_ENABLED == 1)
+#define SPIS2_CONFIG_SCK_PIN         2
+#define SPIS2_CONFIG_MOSI_PIN        3
+#define SPIS2_CONFIG_MISO_PIN        4
+#define SPIS2_CONFIG_IRQ_PRIORITY    APP_IRQ_PRIORITY_LOW
+int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+String IR_DATA;
+unsigned long codeValue; // The code value if not raw
+//RF
+const char *message = "aa Christmas";
+//adf
+int ID_decode = 0;
+
+const int ID_array[8] = {
+  A4, A5, 2, 3, 4, 5, 6, 7
+};
+int ID_data[8];
